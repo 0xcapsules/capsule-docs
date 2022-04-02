cb227203f787f14449e24e63787c415f74159163
Paul Fidika <paul@fidika.com>
2022-04-02 15:02:10 -0600

    configure express to require https

--- packages/express-server/src/main.ts
+++ packages/express-server/src/main.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 import { join } from 'path'
 
+const PORT = process.env.PORT || '3000'
 const app = express()
-const PORT = process.env.PORT || 3000
+
+app.enable('trust proxy')
+app.use((request, response, next) => {
+  if (process.env.NODE_ENV === 'production' && !request.secure) {
+    return response.redirect('https://' + request.headers.host + request.url)
+  }
+  next()
+})
 
 const clientPath = join(__dirname, '../../open-rails-docs/build')
-
 app.use(express.static(clientPath))
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
