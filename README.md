f3ed6939e0bcf1f32b97b0e25711dfb21925137d
Paul Fidika <paul@fidika.com>
2022-04-02 14:39:44 -0600

    rename package folders, add README at root

---
+++ README.md
@@ -0,0 +1,3 @@
+### Welcome
+
+This repo hosts documentation for all OpenRails projects and various improvement-proposals and standards.
--- packages/express-server/src/main.ts
+++ packages/express-server/src/main.ts
@@ -4,7 +4,7 @@ import { join } from 'path'
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const clientPath = join(__dirname, '../../client/build')
+const clientPath = join(__dirname, '../../open-rails-docs/build')
 
 app.use(express.static(clientPath))
 
